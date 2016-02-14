TrinityCore/TrinityCore

commit 52758c1a0b8e37e198d744803460e34600d115b5

Core/Maps: Use map.dbc zone fallback if no area is found in grid files, not only if grid files don't exist
Closes #16583
Closes #16584

Extracting new maps is NOT needed.

(cherry picked from commit fa3f106693733e7db8061fbd44295b0168a94614)

src/server/game/Maps/Map.cpp
@@ -2447,8 +2447,9 @@ uint32 Map::GetAreaId(float x, float y, float z, bool *isOutdoors) const
     {
         if (GridMap* gmap = const_cast<Map*>(this)->GetGrid(x, y))
             areaId = gmap->getArea(x, y);
+
         // this used while not all *.map files generated (instances)
-        else
+        if (!areaId)
             areaId = i_mapEntry->AreaTableID;
     }
 

jairoe05/tc_duplicate

commit 0883de2ce7ef305a3c9660d961b34cdf1cb46308

Fix build failing since https://github.com/TrinityCore/TrinityCore/commit/52758c1a0b8e37e198d744803460e34600d115b5
/TrinityCore/src/server/game/Maps/Map.cpp:2448:22: fatal error: 
      variable 'areaId' is used uninitialized whenever 'if' condition is false
      [-Wsometimes-uninitialized]
        if (GridMap* gmap = const_cast<Map*>(this)->GetGrid(x, y))

src/server/game/Maps/Map.cpp
@@ -2439,7 +2439,7 @@ uint32 Map::GetAreaId(float x, float y, float z, bool *isOutdoors) const
             atEntry = sAreaTableStore.LookupEntry(wmoEntry->AreaTableID);
     }
 
-    uint32 areaId;
+    uint32 areaId = 0;
 
     if (atEntry)
         areaId = atEntry->ID;
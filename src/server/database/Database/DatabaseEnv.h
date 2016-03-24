Naios/TrinityCore

Core/Database: Converted the database library to a shared library. * There is still the possibility to static link against database.

src/server/database/Database/DatabaseEnv.h
@@ -40,12 +40,12 @@
 #include "Implementation/HotfixDatabase.h"
 
 /// Accessor to the world database
-extern WorldDatabaseWorkerPool WorldDatabase;
+TRINITY_DATABASE_API extern WorldDatabaseWorkerPool WorldDatabase;
 /// Accessor to the character database
-extern CharacterDatabaseWorkerPool CharacterDatabase;
+TRINITY_DATABASE_API extern CharacterDatabaseWorkerPool CharacterDatabase;
 /// Accessor to the realm/login database
-extern LoginDatabaseWorkerPool LoginDatabase;
+TRINITY_DATABASE_API extern LoginDatabaseWorkerPool LoginDatabase;
 /// Accessor to the hotfix database
-extern HotfixDatabaseWorkerPool HotfixDatabase;
+TRINITY_DATABASE_API extern HotfixDatabaseWorkerPool HotfixDatabase;
 
 #endif

src/server/database/Database/DatabaseLoader.h
@@ -27,7 +27,7 @@
 
 // A helper class to initiate all database worker pools,
 // handles updating, delays preparing of statements and cleans up on failure.
-class DatabaseLoader
+class TRINITY_DATABASE_API DatabaseLoader
 {
 public:
     DatabaseLoader(std::string const& logger, uint32 const defaultUpdateMask);

src/server/database/Database/DatabaseWorkerPool.cpp
@@ -317,7 +317,7 @@ T* DatabaseWorkerPool<T>::GetFreeConnection()
     return connection;
 }
 
-template class DatabaseWorkerPool<LoginDatabaseConnection>;
-template class DatabaseWorkerPool<WorldDatabaseConnection>;
-template class DatabaseWorkerPool<CharacterDatabaseConnection>;
-template class DatabaseWorkerPool<HotfixDatabaseConnection>;
+template class TRINITY_DATABASE_API DatabaseWorkerPool<LoginDatabaseConnection>;
+template class TRINITY_DATABASE_API DatabaseWorkerPool<WorldDatabaseConnection>;
+template class TRINITY_DATABASE_API DatabaseWorkerPool<CharacterDatabaseConnection>;
+template class TRINITY_DATABASE_API DatabaseWorkerPool<HotfixDatabaseConnection>;

src/server/database/Database/Implementation/WorldDatabase.h
@@ -103,7 +103,7 @@ enum WorldDatabaseStatements
     MAX_WORLDDATABASE_STATEMENTS
 };
 
-class WorldDatabaseConnection : public MySQLConnection
+class TRINITY_DATABASE_API WorldDatabaseConnection : public MySQLConnection
 {
 public:
     typedef WorldDatabaseStatements Statements;

src/server/database/Database/MySQLConnection.h
@@ -35,7 +35,7 @@ enum ConnectionFlags
     CONNECTION_BOTH = CONNECTION_ASYNC | CONNECTION_SYNCH
 };
 
-struct MySQLConnectionInfo
+struct TRINITY_DATABASE_API MySQLConnectionInfo
 {
     explicit MySQLConnectionInfo(std::string const& infoString)
     {
@@ -62,7 +62,7 @@ struct MySQLConnectionInfo
 
 typedef std::map<uint32 /*index*/, std::pair<std::string /*query*/, ConnectionFlags /*sync/async*/> > PreparedStatementMap;
 
-class MySQLConnection
+class TRINITY_DATABASE_API MySQLConnection
 {
     template <class T> friend class DatabaseWorkerPool;
     friend class PingOperation;

src/server/database/Database/Transaction.h
@@ -25,7 +25,7 @@
 class PreparedStatement;
 
 /*! Transactions, high level class. */
-class Transaction
+class TRINITY_DATABASE_API Transaction
 {
     friend class TransactionTask;
     friend class MySQLConnection;
@@ -58,7 +58,7 @@ class Transaction
 typedef std::shared_ptr<Transaction> SQLTransaction;
 
 /*! Low level class*/
-class TransactionTask : public SQLOperation
+class TRINITY_DATABASE_API TransactionTask : public SQLOperation
 {
     template <class T> friend class DatabaseWorkerPool;
     friend class DatabaseWorker;

src/server/database/Logging/AppenderDB.h
@@ -20,7 +20,7 @@
 
 #include "Appender.h"
 
-class AppenderDB: public Appender
+class TRINITY_DATABASE_API AppenderDB: public Appender
 {
     public:
         typedef std::integral_constant<AppenderType, APPENDER_DB>::type TypeIndex;

src/server/database/Updater/DBUpdater.cpp
@@ -468,7 +468,7 @@ void DBUpdater<T>::ApplyFile(DatabaseWorkerPool<T>& pool, std::string const& hos
     }
 }
 
-template class DBUpdater<LoginDatabaseConnection>;
-template class DBUpdater<WorldDatabaseConnection>;
-template class DBUpdater<CharacterDatabaseConnection>;
-template class DBUpdater<HotfixDatabaseConnection>;
+template class TRINITY_DATABASE_API DBUpdater<LoginDatabaseConnection>;
+template class TRINITY_DATABASE_API DBUpdater<WorldDatabaseConnection>;
+template class TRINITY_DATABASE_API DBUpdater<CharacterDatabaseConnection>;
+template class TRINITY_DATABASE_API DBUpdater<HotfixDatabaseConnection>;

jairoe05/tc_duplicate

src/server/database/Database/DatabaseEnv.h
@@ -40,12 +40,12 @@
 #include "Implementation/HotfixDatabase.h"
 
 /// Accessor to the world database
-extern WorldDatabaseWorkerPool WorldDatabase;
+TC_DATABASE_API extern WorldDatabaseWorkerPool WorldDatabase;
 /// Accessor to the character database
-extern CharacterDatabaseWorkerPool CharacterDatabase;
+TC_DATABASE_API extern CharacterDatabaseWorkerPool CharacterDatabase;
 /// Accessor to the realm/login database
-extern LoginDatabaseWorkerPool LoginDatabase;
+TC_DATABASE_API extern LoginDatabaseWorkerPool LoginDatabase;
 /// Accessor to the hotfix database
-extern HotfixDatabaseWorkerPool HotfixDatabase;
+TC_DATABASE_API extern HotfixDatabaseWorkerPool HotfixDatabase;
 
 #endif

src/server/database/Database/DatabaseLoader.h
@@ -27,7 +27,7 @@
 
 // A helper class to initiate all database worker pools,
 // handles updating, delays preparing of statements and cleans up on failure.
-class DatabaseLoader
+class TC_DATABASE_API DatabaseLoader
 {
 public:
     DatabaseLoader(std::string const& logger, uint32 const defaultUpdateMask);

src/server/database/Database/DatabaseWorkerPool.cpp
@@ -317,7 +317,7 @@ T* DatabaseWorkerPool<T>::GetFreeConnection()
     return connection;
 }
 
-template class DatabaseWorkerPool<LoginDatabaseConnection>;
-template class DatabaseWorkerPool<WorldDatabaseConnection>;
-template class DatabaseWorkerPool<CharacterDatabaseConnection>;
-template class DatabaseWorkerPool<HotfixDatabaseConnection>;
+template class TC_DATABASE_API DatabaseWorkerPool<LoginDatabaseConnection>;
+template class TC_DATABASE_API DatabaseWorkerPool<WorldDatabaseConnection>;
+template class TC_DATABASE_API DatabaseWorkerPool<CharacterDatabaseConnection>;
+template class TC_DATABASE_API DatabaseWorkerPool<HotfixDatabaseConnection>;

src/server/database/Database/Implementation/WorldDatabase.h
@@ -103,7 +103,7 @@ enum WorldDatabaseStatements
     MAX_WORLDDATABASE_STATEMENTS
 };
 
-class WorldDatabaseConnection : public MySQLConnection
+class TC_DATABASE_API WorldDatabaseConnection : public MySQLConnection
 {
 public:
     typedef WorldDatabaseStatements Statements;

src/server/database/Database/MySQLConnection.h
@@ -35,7 +35,7 @@ enum ConnectionFlags
     CONNECTION_BOTH = CONNECTION_ASYNC | CONNECTION_SYNCH
 };
 
-struct MySQLConnectionInfo
+struct TC_DATABASE_API MySQLConnectionInfo
 {
     explicit MySQLConnectionInfo(std::string const& infoString)
     {
@@ -62,7 +62,7 @@ struct MySQLConnectionInfo
 
 typedef std::map<uint32 /*index*/, std::pair<std::string /*query*/, ConnectionFlags /*sync/async*/> > PreparedStatementMap;
 
-class MySQLConnection
+class TC_DATABASE_API MySQLConnection
 {
     template <class T> friend class DatabaseWorkerPool;
     friend class PingOperation;

src/server/database/Database/Transaction.h
@@ -25,7 +25,7 @@
 class PreparedStatement;
 
 /*! Transactions, high level class. */
-class Transaction
+class TC_DATABASE_API Transaction
 {
     friend class TransactionTask;
     friend class MySQLConnection;
@@ -58,7 +58,7 @@ class Transaction
 typedef std::shared_ptr<Transaction> SQLTransaction;
 
 /*! Low level class*/
-class TransactionTask : public SQLOperation
+class TC_DATABASE_API TransactionTask : public SQLOperation
 {
     template <class T> friend class DatabaseWorkerPool;
     friend class DatabaseWorker;

src/server/database/Logging/AppenderDB.h
@@ -20,7 +20,7 @@
 
 #include "Appender.h"
 
-class AppenderDB: public Appender
+class TC_DATABASE_API AppenderDB: public Appender
 {
     public:
         typedef std::integral_constant<AppenderType, APPENDER_DB>::type TypeIndex;

src/server/database/Updater/DBUpdater.cpp
@@ -468,7 +468,7 @@ void DBUpdater<T>::ApplyFile(DatabaseWorkerPool<T>& pool, std::string const& hos
     }
 }
 
-template class DBUpdater<LoginDatabaseConnection>;
-template class DBUpdater<WorldDatabaseConnection>;
-template class DBUpdater<CharacterDatabaseConnection>;
-template class DBUpdater<HotfixDatabaseConnection>;
+template class TC_DATABASE_API DBUpdater<LoginDatabaseConnection>;
+template class TC_DATABASE_API DBUpdater<WorldDatabaseConnection>;
+template class TC_DATABASE_API DBUpdater<CharacterDatabaseConnection>;
+template class TC_DATABASE_API DBUpdater<HotfixDatabaseConnection>;